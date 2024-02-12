36e953d7ff9a15ecc9e6c015b0fda51e9aa9b5e8
PBillingsby <philbillingsby@gmail.com>
2024-02-12 13:47:44 -0500

    add script flow comments

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 async function main() {
 	const connection = new Connection('http://127.0.0.1:8899', 'confirmed');
+	// - Generate keys
 	const payer = await initializeKeypair(connection);
 
 	const mintKeypair = Keypair.generate();
@@ -17,9 +18,20 @@ async function main() {
 	const tokenAccountKeypair = Keypair.generate();
 	const tokenAccount = tokenAccountKeypair.publicKey;
 
-	console.log("Payer Account Public Key: ", payer.publicKey)
-	console.log("Mint Public Key: ", mint)
-	console.log("Token Account Public Key: ", tokenAccount)
+	console.log("Payer Account Public Key: ", payer.publicKey);
+	console.log("Mint Public Key: ", mint);
+	console.log("Token Account Public Key: ", tokenAccount);
+
+	// - Create Mint Account
+	// - Create Token Account
+
+	// - Tests
+	// - Minting without thawing
+	// - Thawing and minting
+	// - Trying to transfer ownership
+	// - Trying to transfer without a memo
+	// - Transferring with memo with a frozen account
+	// - Transferring with memo with a thawed account
 }
 
 main();
